bf927956c109ac44d9765b7b269dea98f63ecbd2
BrettS89 <blsodie@gmail.com>
2019-03-05 12:34:31 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text} from 'react-native';
+
+class AppContainer extends React.Component {
+  render () {
+    return (
+      <View></View>
+    )
+  }
+}
+
+export default AppContainer;
